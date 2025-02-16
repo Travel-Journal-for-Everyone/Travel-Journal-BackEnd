2308976c13f97209dc9ea9c08e8c4ef39a583765
전화철 <lunaris0206@naver.com>
2025-02-16 12:27:53 +0900

    Initial settings

---
+++ src/test/java/com/traveljournal/api/TravelJournalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.traveljournal.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelJournalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

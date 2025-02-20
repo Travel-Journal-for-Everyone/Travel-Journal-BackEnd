c4fa35c0b0db155b64a8dbee483ec2845ec1c7b9
전화철 <lunaris0206@naver.com>
2025-02-21 01:45:50 +0900

    feat : add ApiResponse
    응답 값 및 상태 추가
    헤더에 Authorization과 Bearer + AccessToken 삽입

---
+++ src/main/java/com/traveljournal/global/data/ApiResponse.java
@@ -0,0 +1,35 @@
+package com.traveljournal.global.data;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+public class ApiResponse<T> extends ResponseEntity<T> {
+
+	public ApiResponse(HttpStatus status) {
+		super(status);
+	}
+
+	public static <LoginResponse> ResponseEntity<LoginResponse> accessTokenResponse(LoginResponse loginResponse,
+		String accessToken) {
+		return ResponseEntity.ok()
+			.header("Authorization", "Bearer " + accessToken)
+			.body(loginResponse);
+	}
+
+	public static ResponseEntity<?> createdSuccess(String message) {
+		return ResponseEntity.status(HttpStatus.CREATED).body(message);
+	}
+
+	public static ResponseEntity<?> deletedSuccess(String message) {
+		return ResponseEntity.ok(message);
+	}
+
+	public static ResponseEntity<?> updatedSuccess(String message) {
+		return ResponseEntity.ok(message);
+	}
+
+	public static <T> ResponseEntity<T> getObjectSuccess(T object) {
+		return ResponseEntity.ok(object);
+	}
+
+}

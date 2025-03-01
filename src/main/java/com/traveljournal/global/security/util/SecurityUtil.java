b0234c6d94b32b7079950e3e707d90fdf315b6b8
전화철 <lunaris0206@naver.com>
2025-03-02 01:11:03 +0900

    feat : Add getCurrentMemberId

---
+++ src/main/java/com/traveljournal/global/security/util/SecurityUtil.java
@@ -0,0 +1,26 @@
+package com.traveljournal.global.security.util;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import com.traveljournal.global.security.service.CustomUserDetails;
+
+/**
+ * 현재 로그인한 사용자의 정보를 가져오는 유틸 클래스
+ */
+public class SecurityUtil {
+
+	/**
+	 * 현재 로그인한 사용자의 memberId 가져오기
+	 * @return Long memberId
+	 * @throws IllegalStateException 로그인 정보가 없거나 잘못된 경우
+	 */
+	public static Long getCurrentMemberId() {
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+		if (authentication == null || !(authentication.getPrincipal() instanceof CustomUserDetails userDetails)) {
+			throw new IllegalStateException("올바른 사용자 정보가 아닙니다.");
+		}
+		return userDetails.getMemberId();
+	}
+}

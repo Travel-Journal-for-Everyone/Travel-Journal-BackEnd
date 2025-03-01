9d54d9fe9d1d18ddcf9b86e12e1c649a875d1918
전화철 <lunaris0206@naver.com>
2025-03-02 01:10:42 +0900

    chore: add @Component annotation to JwtAuthenticationFilter

--- src/main/java/com/traveljournal/global/security/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/traveljournal/global/security/jwt/JwtAuthenticationFilter.java
@@ -3,6 +3,7 @@ package com.traveljournal.global.security.jwt;
 import java.io.IOException;
 
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import com.traveljournal.global.data.JwtValidateStatus;
@@ -20,6 +21,7 @@ import lombok.extern.slf4j.Slf4j;
  */
 @Slf4j
 @RequiredArgsConstructor
+@Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
 	private final JwtTokenProvider jwtTokenProvider;

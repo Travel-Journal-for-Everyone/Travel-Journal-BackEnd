968a182d74e9857d761ccae6c14c1a7adcffed07
전화철 <lunaris0206@naver.com>
2025-03-13 12:00:09 +0900

    TJFE-61 [refator] toSocialProvider method - Add trim
    맥 유저에서 특이점 발생으로 trim추가
    포스트맨 사용시 앞에 공백문자로 인하여 오류나는점 trim으로 해결

--- src/main/java/com/traveljournal/domain/auth/util/EnumUtils.java
+++ src/main/java/com/traveljournal/domain/auth/util/EnumUtils.java
@@ -5,7 +5,7 @@ import com.traveljournal.domain.member.entity.SocialProvider;
 public class EnumUtils {
 	public static SocialProvider toSocialProvider(String socialProviderStr) {
 		try {
-			return SocialProvider.valueOf(socialProviderStr.toUpperCase());
+			return SocialProvider.valueOf(socialProviderStr.trim().toUpperCase());
 		} catch (IllegalArgumentException e) {
 			throw new IllegalArgumentException("Invalid SocialProvider: " + socialProviderStr, e);
 		}

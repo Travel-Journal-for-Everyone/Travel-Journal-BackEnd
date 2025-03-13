4bcd8836eb6ee50b6367d360dbb6aa97f3164023
전화철 <lunaris0206@naver.com>
2025-03-13 14:36:09 +0900

    TJFE-61 [refactor] Remove unused fields from ReissueResponse

--- src/main/java/com/traveljournal/domain/member/dto/ReissueResponse.java
+++ src/main/java/com/traveljournal/domain/member/dto/ReissueResponse.java
@@ -5,21 +5,12 @@ import lombok.Builder;
 
 @Builder
 public record ReissueResponse(
-	@Schema(example = "1")
-	Long memberId,
-	@Schema(example = "true")
-	String email,
 	@Schema(example = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJsdW5hcmlzMDIwNkBuYXZlci5jb20iLCJpYXE3NDMsImV4cCI6MTc0MjIwMDAzM30.nf5sYgC3nQ-ysA3yRdHBpTXjzSdyIsIGQWxdYeT1tt8")
-	String refreshToken,
-	@Schema(example = "8bd0476d-e790-4133-bbfe-3a90f6757537")
-	String deviceId
+	String refreshToken
 ) {
 	public static ReissueResponse of(MemberTokenResponse memberTokenResponse) {
 		return ReissueResponse.builder()
-			.memberId(memberTokenResponse.memberId())
-			.email(memberTokenResponse.email())
 			.refreshToken(memberTokenResponse.tokenInfo().refreshToken())
-			.deviceId(memberTokenResponse.tokenInfo().deviceId())
 			.build();
 	}
 }

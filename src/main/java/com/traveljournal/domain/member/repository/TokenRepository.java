e819bc63a17048b566d3f31403932c1e1dc22fe0
전화철 <lunaris0206@naver.com>
2025-03-31 15:22:31 +0900

    TJFE-105 [refactor] Implement bulk deletion for tokens

--- src/main/java/com/traveljournal/domain/member/repository/TokenRepository.java
+++ src/main/java/com/traveljournal/domain/member/repository/TokenRepository.java
@@ -3,6 +3,9 @@ package com.traveljournal.domain.member.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.traveljournal.domain.member.entity.Token;
 
@@ -12,5 +15,7 @@ public interface TokenRepository extends JpaRepository<Token, Long> {
 
 	Optional<Token> findByDeviceIdAndMemberId(String deviceId, Long memberId);
 
-	void deleteAllByMemberId(Long memberId);
+	@Modifying
+	@Query("DELETE FROM Token t WHERE t.member.id = :memberId")
+	void deleteAllByMemberId(@Param("memberId") Long memberId);
 }

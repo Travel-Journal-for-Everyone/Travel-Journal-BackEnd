fa520bfdc6236397e439b9417fa4ede319381dfb
전화철 <lunaris0206@naver.com>
2025-04-14 15:21:17 +0900

    fix : Resolve PageImpl serialization warning

--- src/main/java/com/traveljournal/TravelJournalApplication.java
+++ src/main/java/com/traveljournal/TravelJournalApplication.java
@@ -3,7 +3,9 @@ package com.traveljournal;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 @EnableFeignClients
 @SpringBootApplication
 public class TravelJournalApplication {

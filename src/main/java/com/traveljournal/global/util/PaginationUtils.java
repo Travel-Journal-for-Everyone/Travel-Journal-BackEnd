4f83eb3aa83e00f0ee846c5993498f44e9051f61
전화철 <lunaris0206@naver.com>
2025-04-14 21:46:53 +0900

    feat : implement empty array

--- src/main/java/com/traveljournal/global/util/PaginationUtils.java
+++ src/main/java/com/traveljournal/global/util/PaginationUtils.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 
 public class PaginationUtils {
@@ -18,7 +19,7 @@ public class PaginationUtils {
 	 */
 	public static <T> Page<T> getPagedList(List<T> allData, Pageable pageable) {
 		if (allData == null || allData.isEmpty() || pageable == null) {
-			return new PageImpl<>(Collections.emptyList(), Pageable.unpaged(), 0);
+			return Page.empty(pageable != null ? pageable : PageRequest.of(0, 10));
 		}
 
 		int start = (int) pageable.getOffset();

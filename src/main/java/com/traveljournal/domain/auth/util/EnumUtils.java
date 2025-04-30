9c5a4e62eec0511ca8ee49fbb5c84b3088f90e6b
전화철 <lunaris0206@naver.com>
2025-04-30 12:00:45 +0900

    feat : Apply BadRequestException for EnumUtils

--- src/main/java/com/traveljournal/domain/auth/util/EnumUtils.java
+++ src/main/java/com/traveljournal/domain/auth/util/EnumUtils.java
@@ -1,13 +1,14 @@
 package com.traveljournal.domain.auth.util;
 
 import com.traveljournal.domain.member.entity.SocialProvider;
+import com.traveljournal.global.exception.BadRequestException;
 
 public class EnumUtils {
 	public static SocialProvider toSocialProvider(String socialProviderStr) {
 		try {
 			return SocialProvider.valueOf(socialProviderStr.trim().toUpperCase());
 		} catch (IllegalArgumentException e) {
-			throw new IllegalArgumentException("Invalid SocialProvider: " + socialProviderStr, e);
+			throw new BadRequestException("지원하지 않는 소숄 로그인 제공자입니다 : " + socialProviderStr);
 		}
 	}
 }

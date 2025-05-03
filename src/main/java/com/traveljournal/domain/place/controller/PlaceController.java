1d1c0e3149847f61f9d64f9a173775483d043ba7
전화철 <lunaris0206@naver.com>
2025-05-03 17:17:11 +0900

    TJFE-162 [docs] : add example usage to DTO and page parameter

--- src/main/java/com/traveljournal/domain/place/controller/PlaceController.java
+++ src/main/java/com/traveljournal/domain/place/controller/PlaceController.java
@@ -1,5 +1,6 @@
 package com.traveljournal.domain.place.controller;
 
+import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
@@ -14,6 +15,7 @@ import com.traveljournal.domain.place.service.PlaceService;
 import com.traveljournal.global.data.ApiResponseHandler;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
@@ -33,9 +35,11 @@ public class PlaceController {
 	)
 	@GetMapping("/members/{memberId}/places/region/{regionName}")
 	public ResponseEntity<Page<PlaceListResponse>> findPlacesByRegionPaged(
+		@Parameter(description = "조회할 member_id")
 		@PathVariable Long memberId,
+		@Parameter(description = "수도권, 강원도, 충정도, 경상도, 전라도, 제주도")
 		@PathVariable String regionName,
-		@PageableDefault Pageable pageable) {
+		@ParameterObject @PageableDefault Pageable pageable) {
 		return ApiResponseHandler.getObjectSuccess(placeService.findPlacesByRegionWithPagion(memberId, regionName, pageable));
 	}
 
@@ -46,8 +50,9 @@ public class PlaceController {
 	)
 	@GetMapping("/members/{memberId}/places")
 	public ResponseEntity<Page<PlaceListResponse>> findJournalsByMember(
+		@Parameter(description = "조회할 member_id")
 		@PathVariable Long memberId,
-		@PageableDefault Pageable pageable) {
+		@ParameterObject @PageableDefault Pageable pageable) {
 		return ApiResponseHandler.getObjectSuccess(placeService.findAllPlacesByMemberId(memberId, pageable));
 	}
 }
--- src/main/java/com/traveljournal/domain/place/dto/PlaceListResponse.java
+++ src/main/java/com/traveljournal/domain/place/dto/PlaceListResponse.java
@@ -1,9 +1,15 @@
 package com.traveljournal.domain.place.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 public record PlaceListResponse(
+	@Schema(example = "1")
 	Long placeId,
+	@Schema(example = "서울 타워")
 	String title,
+	@Schema(example = "서울")
 	String region,
+	@Schema(example = "https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyNTAzMjFfMTEw%2FMDAxNzQyNTU5MjY0OTkz.US8DxCfatYon23fMlPjPlqGIvpK8Zd8SIP3BuNFyGmUg.LUZS2bZBQ1aJuHyVI52EjhzHykDFewCj4mpJCeoV0G0g.JPEG%2F2025%25BA%25A2%25B2%25C9%25B0%25B3%25C8%25AD%25BD%25C3%25B1%25E2IMG_2503-009.JPG&type=sc960_832")
 	String thumbnailUrl
 ) {
 }

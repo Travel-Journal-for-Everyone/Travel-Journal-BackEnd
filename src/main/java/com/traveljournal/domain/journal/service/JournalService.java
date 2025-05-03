86d7f87bf85286d3f806518e5c50d25c01bef017
전화철 <lunaris0206@naver.com>
2025-05-04 01:05:40 +0900

    TJFE-162 [feat] Create Journal entity and populate table with real data

--- src/main/java/com/traveljournal/domain/journal/service/JournalService.java
+++ src/main/java/com/traveljournal/domain/journal/service/JournalService.java
@@ -1,38 +1,64 @@
 package com.traveljournal.domain.journal.service;
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.traveljournal.domain.hashtag.entity.HashTag;
 import com.traveljournal.domain.journal.dto.JournalListResponse;
-import com.traveljournal.global.dummy.DummyDataProvider;
-import com.traveljournal.global.util.PaginationUtils;
+import com.traveljournal.domain.journal.entity.Journal;
+import com.traveljournal.domain.journal.repository.JournalRepository;
+import com.traveljournal.global.util.RegionGroupUtil;
 
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class JournalService {
-	private final DummyDataProvider dummyDataProvider;
+
+	private final JournalRepository journalRepository;
 
 	@Transactional(readOnly = true)
-	public List<JournalListResponse> findJournalsByRegion(String regionName) {
-		return dummyDataProvider.getDummyJournalsByRegion(regionName);
+	public Page<JournalListResponse> findJournalsByRegionWithPaging(Long memberId, String regionName, Pageable pageable) {
+		List<String> regionList = RegionGroupUtil.getRegionList(regionName);
+		Page<Long> journalIdPage = journalRepository.findIdsByMemberIdAndRegionIn(memberId, regionList, pageable);
+		return getJournalListResponses(pageable, journalIdPage);
 	}
 
 	@Transactional(readOnly = true)
-	public Page<JournalListResponse> findJournalsByRegionWithPaging(String regionName, Pageable pageable) {
-		List<JournalListResponse> allData = findJournalsByRegion(regionName);
-		return PaginationUtils.getPagedList(allData, pageable);
+	public Page<JournalListResponse> findAllJournalsByMemberId(Long memberId, Pageable pageable) {
+		Page<Long> journalIdPage = journalRepository.findIdsByMemberId(memberId, pageable);
+		return getJournalListResponses(pageable, journalIdPage);
 	}
 
-	// MemberId추후 구현 필요
-	@Transactional(readOnly = true)
-	public Page<JournalListResponse> findAllJournalsByMemberId(Pageable pageable) {
-		List<JournalListResponse> allData = findJournalsByRegion("all");
-		return PaginationUtils.getPagedList(allData, pageable);
+	private Page<JournalListResponse> getJournalListResponses(Pageable pageable, Page<Long> journalIdPage) {
+		List<Long> journalIds = journalIdPage.getContent();
+
+		List<Journal> journals = journalRepository.findAllByIdInFetchJoin(journalIds);
+
+		Map<Long, Journal> journalMap = journals.stream().collect(Collectors.toMap(Journal::getId, j -> j));
+		List<Journal> sortedJournals = journalIds.stream().map(journalMap::get).toList();
+
+		return new PageImpl<>(
+			sortedJournals.stream()
+				.map(journal -> new JournalListResponse(
+					journal.getId(),
+					journal.getHashTags().stream().map(HashTag::getTagName).toList(),
+					journal.getTitle(),
+					journal.getNights(),
+					journal.getDays(),
+					journal.getStartDate(),
+					journal.getEndDate()
+				))
+				.toList(),
+			pageable,
+			journalIdPage.getTotalElements()
+		);
 	}
 }
--- src/main/java/com/traveljournal/domain/member/entity/Member.java
+++ src/main/java/com/traveljournal/domain/member/entity/Member.java
@@ -4,6 +4,7 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.traveljournal.domain.journal.entity.Journal;
 import com.traveljournal.domain.place.entity.Place;
 
 import jakarta.persistence.Column;
@@ -61,6 +62,9 @@ public class Member {
 
 	@OneToMany(mappedBy = "member", fetch = FetchType.LAZY)
 	private List<Place> places = new ArrayList<>();
+
+	@OneToMany(mappedBy = "member", fetch = FetchType.LAZY)
+	private List<Journal> journals = new ArrayList<>();
 
 	@PrePersist
 	public void prePersist() {

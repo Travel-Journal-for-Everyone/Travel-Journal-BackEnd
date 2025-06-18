d1dacb02b71452ab4a0aa5e92d73912d309bed96
전화철 <lunaris0206@naver.com>
2025-06-18 20:44:18 +0900

    feat: Add findByFilename method to ImageInfoRepository

--- src/main/java/com/traveljournal/domain/Image/repository/ImageInfoRepository.java
+++ src/main/java/com/traveljournal/domain/Image/repository/ImageInfoRepository.java
@@ -1,8 +1,11 @@
 package com.traveljournal.domain.Image.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.traveljournal.domain.Image.entity.ImageInfo;
 
 public interface ImageInfoRepository extends JpaRepository<ImageInfo, Long> {
+	Optional<ImageInfo> findByFilename(String filename);
 }

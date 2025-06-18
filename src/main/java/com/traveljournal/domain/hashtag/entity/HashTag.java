c6a241526e749efbe215183e839863d8ed369546
전화철 <lunaris0206@naver.com>
2025-06-18 20:42:32 +0900

    feat : Add static factory method 'of' to HashTag

--- src/main/java/com/traveljournal/domain/hashtag/entity/HashTag.java
+++ src/main/java/com/traveljournal/domain/hashtag/entity/HashTag.java
@@ -7,6 +7,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -14,6 +15,7 @@ import lombok.NoArgsConstructor;
 @Table(name = "hashtag")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 public class HashTag {
 
 	@Id
@@ -22,4 +24,8 @@ public class HashTag {
 
 	@Column(unique = true, nullable = false)
 	private String tagName;
+
+	public static HashTag of(String tagName) {
+		return new HashTag(null, tagName);
+	}
 }

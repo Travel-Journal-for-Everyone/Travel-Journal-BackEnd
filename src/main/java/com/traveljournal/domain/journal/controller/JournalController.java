7fb92c5ae83950f2e63e836a4a14373ce9c4102d
전화철 <lunaris0206@naver.com>
2025-06-18 20:45:47 +0900

    feat: Add createJournal endpoint to JournalController

--- src/main/java/com/traveljournal/domain/journal/controller/JournalController.java
+++ src/main/java/com/traveljournal/domain/journal/controller/JournalController.java
@@ -7,17 +7,22 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.traveljournal.domain.journal.dto.JournalCreateRequest;
 import com.traveljournal.domain.journal.dto.JournalListResponse;
 import com.traveljournal.domain.journal.service.JournalService;
 import com.traveljournal.global.data.ApiResponseHandler;
+import com.traveljournal.global.security.util.SecurityUtil;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -40,7 +45,8 @@ public class JournalController {
 		@Parameter(description = "수도권, 강원도, 충정도, 경상도, 전라도, 제주도")
 		@PathVariable String regionName,
 		@ParameterObject @PageableDefault Pageable pageable) {
-		return ApiResponseHandler.getObjectSuccess(journalService.findJournalsByRegionWithPaging(memberId, regionName, pageable));
+		return ApiResponseHandler.getObjectSuccess(
+			journalService.findJournalsByRegionWithPaging(memberId, regionName, pageable));
 	}
 
 	@Operation(
@@ -54,5 +60,20 @@ public class JournalController {
 		@PathVariable Long memberId,
 		@ParameterObject @PageableDefault Pageable pageable) {
 		return ApiResponseHandler.getObjectSuccess(journalService.findAllJournalsByMemberId(memberId, pageable));
+	}
+
+	@PostMapping(value = "/members/journal/create")
+	@Operation(
+		summary = "여행일지 작성",
+		description = "여행일지, 일차, 방문지, 사진 메타데이터를 한 번에 저장합니다.",
+		security = @SecurityRequirement(name = "bearer-key")
+	)
+	public ResponseEntity<?> createJournal(
+		@RequestBody @Valid JournalCreateRequest journalCreateRequest
+	) {
+		Long currentMemberId = SecurityUtil.getCurrentMemberId();
+
+		Long journalId = journalService.createJournal(journalCreateRequest, currentMemberId);
+		return ApiResponseHandler.onSuccess("여행일지 작성 성공 journal_id: " + journalId);
 	}
 }

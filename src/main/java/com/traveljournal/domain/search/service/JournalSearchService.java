2cfcfdea89233d4a047a1c2b682f860a2f1c41d2
전화철 <lunaris0206@naver.com>
2025-06-18 23:00:22 +0900

    refactor : Refactor JournalSearchService formatting and stream usage

--- src/main/java/com/traveljournal/domain/search/service/JournalSearchService.java
+++ src/main/java/com/traveljournal/domain/search/service/JournalSearchService.java
@@ -4,18 +4,18 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-import com.traveljournal.domain.block.repository.BlockRepository;
-import com.traveljournal.global.security.util.SecurityUtil;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.traveljournal.domain.block.repository.BlockRepository;
 import com.traveljournal.domain.hashtag.entity.HashTag;
 import com.traveljournal.domain.journal.dto.JournalListResponse;
 import com.traveljournal.domain.journal.entity.Journal;
 import com.traveljournal.domain.journal.repository.JournalRepository;
+import com.traveljournal.global.security.util.SecurityUtil;
 
 import lombok.RequiredArgsConstructor;
 
@@ -62,29 +62,29 @@ public class JournalSearchService {
 		Long currentMemberId = SecurityUtil.getCurrentMemberId();
 		List<Long> blockedIds = blockRepository.findBlockedMemberIdsByBlockerId(currentMemberId);
 
-		Page<Long> journalIdPage = journalRepository.findIdsByKeywordExcludingBlockedMembers(keyword, blockedIds, pageable);
+		Page<Long> journalIdPage = journalRepository.findIdsByKeywordExcludingBlockedMembers(keyword, blockedIds,
+			pageable);
 		List<Long> journalIds = journalIdPage.getContent();
 		List<Journal> journals = journalRepository.findAllByIdInFetchJoin(journalIds);
 
 		Map<Long, Journal> journalMap = journals.stream()
-				.collect(Collectors.toMap(Journal::getId, j -> j));
+			.collect(Collectors.toMap(Journal::getId, j -> j));
 		List<Journal> sortedJournals = journalIds.stream()
-				.map(journalMap::get)
-				.toList();
-
+			.map(journalMap::get)
+			.toList();
 
 		return new PageImpl<>(
 			sortedJournals.stream()
-					.map(journal -> new JournalListResponse(
-							journal.getId(),
-							journal.getHashTags().stream().map(HashTag::getTagName).toList(),
-							journal.getTitle(),
-							journal.getNights(),
-							journal.getDays(),
-							journal.getStartDate(),
-							journal.getEndDate()
-					))
-					.toList(),
+				.map(journal -> new JournalListResponse(
+					journal.getId(),
+					journal.getHashTags().stream().map(HashTag::getTagName).toList(),
+					journal.getTitle(),
+					journal.getNights(),
+					journal.getDays(),
+					journal.getStartDate(),
+					journal.getEndDate()
+				))
+				.toList(),
 			pageable,
 			journalIdPage.getTotalElements()
 		);

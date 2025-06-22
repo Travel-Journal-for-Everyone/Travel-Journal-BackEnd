b81083acbfc99a940d5c3970e17f9e810bcd673b
전화철 <lunaris0206@naver.com>
2025-06-23 03:01:27 +0900

    feat: Add increaseFollow and decreaseFollow method to MemberStatisticsService

--- src/main/java/com/traveljournal/domain/statistics/service/MemberStatisticsService.java
+++ src/main/java/com/traveljournal/domain/statistics/service/MemberStatisticsService.java
@@ -13,10 +13,38 @@ import lombok.RequiredArgsConstructor;
 public class MemberStatisticsService {
 	private final MemberStatisticsRepository memberStatisticsRepository;
 
+	private MemberStatistics findByMemberId(Long memberId) {
+		return memberStatisticsRepository.findById(memberId)
+			.orElseThrow(() -> new IllegalArgumentException("통계 정보 없음"));
+	}
+
 	@Transactional
 	public void increaseTravelDiaryCount(Long memberId) {
-		MemberStatistics stats = memberStatisticsRepository.findById(memberId)
-			.orElseThrow(() -> new IllegalArgumentException("통계 정보 없음"));
+		MemberStatistics stats = findByMemberId(memberId);
 		stats.increaseTravelDiaryCount();
+	}
+
+	@Transactional
+	public void increaseFollowerCount(Long memberId) {
+		MemberStatistics stats = findByMemberId(memberId);
+		stats.increaseFollowerCount();
+	}
+
+	@Transactional
+	public void increaseFollowingCount(Long memberId) {
+		MemberStatistics stats = findByMemberId(memberId);
+		stats.increaseFollowingCount();
+	}
+
+	@Transactional
+	public void decreaseFollowerCount(Long memberId) {
+		MemberStatistics stats = findByMemberId(memberId);
+		stats.decreaseFollowerCount();
+	}
+
+	@Transactional
+	public void decreaseFollowingCount(Long memberId) {
+		MemberStatistics stats = findByMemberId(memberId);
+		stats.decreaseFollowingCount();
 	}
 }

d3b38e33af26fc5a183ce6b1bc5bdfb9b84ed6da
전화철 <lunaris0206@naver.com>
2025-06-23 00:08:43 +0900

    feat: Add static of() method to RegionInfo

--- src/main/java/com/traveljournal/domain/memberDashboard/dto/RegionInfo.java
+++ src/main/java/com/traveljournal/domain/memberDashboard/dto/RegionInfo.java
@@ -1,8 +1,18 @@
 package com.traveljournal.domain.memberDashboard.dto;
 
+import lombok.Builder;
+
+@Builder
 public record RegionInfo(
 	String regionName,
 	Long travelDiaryCount,
 	Long placesCount
 ) {
+	public static RegionInfo of(String regionName, Long travelDiaryCount, Long placesCount) {
+		return RegionInfo.builder()
+			.regionName(regionName)
+			.travelDiaryCount(travelDiaryCount)
+			.placesCount(placesCount)
+			.build();
+	}
 }

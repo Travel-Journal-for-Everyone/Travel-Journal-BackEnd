95234aa2a96548f8c6031139544d6a7e31882e05
전화철 <lunaris0206@naver.com>
2025-06-23 00:57:46 +0900

    refactor: Simplify RegionInfo.of() method to use record constructor

--- src/main/java/com/traveljournal/domain/memberDashboard/dto/RegionInfo.java
+++ src/main/java/com/traveljournal/domain/memberDashboard/dto/RegionInfo.java
@@ -1,18 +1,11 @@
 package com.traveljournal.domain.memberDashboard.dto;
 
-import lombok.Builder;
-
-@Builder
 public record RegionInfo(
 	String regionName,
 	Long travelDiaryCount,
 	Long placesCount
 ) {
 	public static RegionInfo of(String regionName, Long travelDiaryCount, Long placesCount) {
-		return RegionInfo.builder()
-			.regionName(regionName)
-			.travelDiaryCount(travelDiaryCount)
-			.placesCount(placesCount)
-			.build();
+		return new RegionInfo(regionName, travelDiaryCount, placesCount);
 	}
 }

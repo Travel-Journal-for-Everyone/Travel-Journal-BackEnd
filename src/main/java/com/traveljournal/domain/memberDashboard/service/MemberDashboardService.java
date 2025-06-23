4022b5837007d1452f06b514184468693a22e108
전화철 <lunaris0206@naver.com>
2025-06-23 19:18:50 +0900

    feat: Add BlockRelationType to getMemberDashbord method

--- src/main/java/com/traveljournal/domain/memberDashboard/service/MemberDashboardService.java
+++ src/main/java/com/traveljournal/domain/memberDashboard/service/MemberDashboardService.java
@@ -7,14 +7,17 @@ import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 
+import com.traveljournal.domain.block.dto.BlockRelationType;
 import com.traveljournal.domain.block.service.BlockService;
 import com.traveljournal.domain.journal.repository.JournalRepository;
 import com.traveljournal.domain.member.dto.MemberProfileResponse;
+import com.traveljournal.domain.member.entity.Member;
 import com.traveljournal.domain.member.service.MemberService;
 import com.traveljournal.domain.memberDashboard.dto.MemberDashbordResponse;
 import com.traveljournal.domain.memberDashboard.dto.RegionInfo;
 import com.traveljournal.domain.place.repository.PlaceRepository;
 import com.traveljournal.domain.statistics.entity.MemberRegionStatistics;
+import com.traveljournal.domain.statistics.entity.MemberStatistics;
 import com.traveljournal.domain.statistics.repository.MemberRegionStatisticsRepository;
 import com.traveljournal.global.util.RegionGroupUtil;
 
@@ -34,9 +37,13 @@ public class MemberDashboardService {
 
 	public MemberDashbordResponse getMemberDashbord(Long memberId, Long viewerId) {
 
-		blockService.validateNotBlocked(viewerId, memberId);
+		Member member = memberService.findById(memberId);
 
-		MemberProfileResponse memberProfileResponse = memberService.getMemberProfile(memberId);
+		MemberStatistics memberStatistics = memberService.getMemberStatistics(memberId);
+
+		BlockRelationType blockRelationType = blockService.getBlockRelation(viewerId, memberId);
+
+		MemberProfileResponse memberProfileResponse = MemberProfileResponse.of(member, memberStatistics, blockRelationType);
 
 		List<RegionInfo> regionInfos = getRegionStatistics(memberId);
 

277093cb6ad31f14fa26448fb697958bf21634de
전화철 <lunaris0206@naver.com>
2025-06-23 20:32:27 +0900

    feat: Add Swagger Example to MemberProfileResponse

--- src/main/java/com/traveljournal/domain/member/dto/MemberProfileResponse.java
+++ src/main/java/com/traveljournal/domain/member/dto/MemberProfileResponse.java
@@ -5,18 +5,28 @@ import com.traveljournal.domain.member.entity.AccountScope;
 import com.traveljournal.domain.member.entity.Member;
 import com.traveljournal.domain.statistics.entity.MemberStatistics;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
 @Builder
 public record MemberProfileResponse (
+	@Schema(example = "도요새")
 	String nickname,
+	@Schema(example = "https://travel-journal-s3.s3.amazonaws.com/default/profile/default1.png")
 	String profileImageUrl,
+	@Schema(example = "PUBLIC")
 	AccountScope accountScope,
+	@Schema(example = "1")
 	Long followingCount,
+	@Schema(example = "12")
 	Long followerCount,
+	@Schema(example = "53")
 	Long travelDiaryCount,
+	@Schema(example = "0")
 	Long placesCount,
+	@Schema(example = "false")
 	Boolean isFirstLogin,
+	@Schema(allowableValues = {"NONE", "BLOCKED_BY_ME", "BLOCKED_ME", "MUTUAL_BLOCK"})
 	BlockRelationType blockRelationType
 ) {
 

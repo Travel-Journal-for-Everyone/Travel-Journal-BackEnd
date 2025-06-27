6faa29b58c1a94a637b13623712c6840aa3b143d
전화철 <lunaris0206@naver.com>
2025-06-28 01:49:03 +0900

    feat: Add photoOrder to PhotoUrlResponse

--- src/main/java/com/traveljournal/domain/photo/dto/PhotoUrlResponse.java
+++ src/main/java/com/traveljournal/domain/photo/dto/PhotoUrlResponse.java
@@ -8,12 +8,15 @@ public record PhotoUrlResponse(
 	@Schema(example = "journal_photo_20_1750242032088_99a07d36.jpeg")
 	String uploadId,
 	@Schema(example = "https://travel-journal-s3.s3.amazonaws.com/source/journal_photo_20_1750242032088_99a07d36.jpeg")
-	String photoUrl
+	String photoUrl,
+	@Schema(example = "1")
+	Integer photoOrder
 ) {
-	public static PhotoUrlResponse from(ImageInfo imageInfo, String photoUrl) {
+	public static PhotoUrlResponse from(ImageInfo imageInfo, String photoUrl, Integer photoOrder) {
 		return new PhotoUrlResponse(
 			imageInfo.getFilename(),
-			photoUrl
+			photoUrl,
+			photoOrder
 		);
 	}
 }
